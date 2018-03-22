13066152244b03ff36a4a864eb23cf96e762b55a
baolijn <libao_fighting@163.com>
2018-03-22 13:29:25 +0800

    append slice

--- slices.go
+++ slices.go
@@ -35,5 +35,13 @@ func main() {
 	s1 = arr[2:6]
 	fmt.Printf("s1=%v, len(s1)=%d, cap(s1)=%d\n", s1, len(s1), cap(s1))
 	s2 = s1[3:5]
+	fmt.Printf("s2=%v, len(s2)=%d, cap(s2)=%d\n", s2, len(s2), cap(s2))
 	fmt.Println(s2)
+
+	fmt.Println("Slice append")
+	s3 := append(s2, 10)
+	s4 := append(s3, 11)
+	s5 := append(s4, 12)
+	fmt.Println("s3, s4, s5 :", s3, s4, s5)
+	fmt.Println(arr)
 }
