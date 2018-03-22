29d60f32fbd4a95c998a700a235e464eb7c55ac2
baolijn <libao_fighting@163.com>
2018-03-22 14:24:12 +0800

    add sliceops

--- sliceops.go
+++ sliceops.go
@@ -22,4 +22,23 @@ func main() {
 	s3 := make([]int, 10, 32)
 	pringSlice(s3)
 
+	fmt.Println("Copying slice")
+	copy(s2, s1)
+	pringSlice(s2)
+
+	fmt.Println("Deleting elements from slice")
+	s2 = append(s2[:3], s2[4:]...)
+	pringSlice(s2)
+
+	fmt.Println("Popping from front")
+	front := s2[0]
+	s2 = s2[1:]
+	fmt.Println(front)
+	pringSlice(s2)
+
+	fmt.Println("Popping from tail")
+	tail := s2[len(s2) - 1]
+	s2 = s2[:len(s2) - 1]
+	fmt.Println(tail)
+	pringSlice(s2)
 }
