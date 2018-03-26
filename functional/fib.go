0e8419a39125981baecae902d36c572e0a5fcbf3
baolijn <libao_fighting@163.com>
2018-03-26 18:17:19 +0800

    implement interface for func

--- functional/fib.go
+++ functional/fib.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io"
+	"bufio"
+)
 
 //F(0)=0，F(1)=1, F(n)=F(n-1)+F(n-2)（n>=2，n∈N*）
 
-func fibonacci() func() int  {
+func fibonacci() intGen  {
 	a, b := 0, 1
 	return func() int {
 		a, b = b, a +b
@@ -12,9 +17,27 @@ func fibonacci() func() int  {
 	}
 }
 
+type intGen func() int
+
+func (g intGen) Read(p []byte) (n int, err error) {
+	next := g()
+	if next > 10000{
+		return 0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)
+	return strings.NewReader(s).Read(p)
+}
+
+func printFileContents(reader io.Reader)  {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan(){
+		fmt.Println(scanner.Text())
+	}
+}
+
 func main() {
 	f := fibonacci()
-
+	printFileContents(f)
 	fmt.Println(f())
 	fmt.Println(f())
 	fmt.Println(f())
