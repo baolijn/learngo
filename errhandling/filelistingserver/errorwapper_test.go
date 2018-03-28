78f501d5ca6b9d4c79861b7d2ec908f9a3e4bc07
baolijn <libao_fighting@163.com>
2018-03-28 16:16:39 +0800

    add TestErrWrapper test

---
+++ errhandling/filelistingserver/errorwapper_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"io/ioutil"
+	"strings"
+)
+
+type testingUserError string
+
+func (e testingUserError) Error() string {
+	return e.Message()
+}
+
+func (e testingUserError) Message() string {
+	return string(e)
+}
+func errorPanic(writer http.ResponseWriter, request *http.Request) error{
+	panic(123)
+}
+
+func errUserError(writer http.ResponseWriter, request *http.Request) error{
+	return testingUserError("user error")
+}
+
+func TestErrWrapper(t *testing.T)  {
+	tests := []struct{
+		h appHandler
+		code int
+		message string
+	}{
+		{errorPanic, 500, "Internal Server Error"},
+		{errUserError, 400, "user error"},
+	}
+
+	for _, tt := range tests{
+		f := errorWrapper(tt.h)
+		response := httptest.NewRecorder()
+		request := httptest.NewRequest(
+			http.MethodGet,
+			"http://baidu.com",
+			nil,
+			)
+		f(response, request)
+		b, _ := ioutil.ReadAll(response.Body)
+		body := strings.Trim(string(b), "\n")
+		if response.Code != tt.code || body != tt.message {
+			t.Errorf("expect (%d, %s) " +
+				"got (%d, %s)",
+					tt.code, tt.message,
+						response.Code, body)
+		}
+	}
+}
