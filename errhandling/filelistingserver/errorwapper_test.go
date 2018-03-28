9918636c73b700112121b50c228535b95ffd4148
baolijn <libao_fighting@163.com>
2018-03-28 17:01:23 +0800

    add TestErrWrapperInServer and refine codes

--- errhandling/filelistingserver/errorwapper_test.go
+++ errhandling/filelistingserver/errorwapper_test.go
@@ -20,15 +20,15 @@ func (e testingUserError) Error() string {
 func (e testingUserError) Message() string {
 	return string(e)
 }
-func errorPanic(writer http.ResponseWriter, request *http.Request) error{
+func errorPanic(writer http.ResponseWriter, request *http.Request) error {
 	panic(123)
 }
 
-func errUserError(writer http.ResponseWriter, request *http.Request) error{
+func errUserError(writer http.ResponseWriter, request *http.Request) error {
 	return testingUserError("user error")
 }
 
-func errNotFound(writer http.ResponseWriter, request *http.Request) error{
+func errNotFound(writer http.ResponseWriter, request *http.Request) error {
 	return os.ErrNotExist
 }
 
@@ -44,40 +44,53 @@ func errUnknown(_ http.ResponseWriter,
 
 func noError(writer http.ResponseWriter,
 	_ *http.Request) error {
-		fmt.Fprintln(writer, "no error")
+	fmt.Fprintln(writer, "no error")
 	return nil
 }
 
-func TestErrWrapper(t *testing.T)  {
-	tests := []struct{
-		h appHandler
-		code int
-		message string
-	}{
-		{errorPanic, 500, "Internal Server Error"},
-		{errUserError, 400, "user error"},
-		{errNotFound, 404, "Not Found"},
-		{errNoPermission, 403, "Forbidden"},
-		{errUnknown, 500, "Internal Server Error"},
-		{noError, 200, "no error"},
-	}
+var tests = []struct {
+	h       appHandler
+	code    int
+	message string
+}{
+	{errorPanic, 500, "Internal Server Error"},
+	{errUserError, 400, "user error"},
+	{errNotFound, 404, "Not Found"},
+	{errNoPermission, 403, "Forbidden"},
+	{errUnknown, 500, "Internal Server Error"},
+	{noError, 200, "no error"},
+}
 
-	for _, tt := range tests{
+func TestErrWrapper(t *testing.T) {
+	for _, tt := range tests {
 		f := errorWrapper(tt.h)
 		response := httptest.NewRecorder()
 		request := httptest.NewRequest(
 			http.MethodGet,
 			"http://baidu.com",
 			nil,
-			)
+		)
 		f(response, request)
-		b, _ := ioutil.ReadAll(response.Body)
-		body := strings.Trim(string(b), "\n")
-		if response.Code != tt.code || body != tt.message {
-			t.Errorf("expect (%d, %s) " +
-				"got (%d, %s)",
-					tt.code, tt.message,
-						response.Code, body)
-		}
+		verifyResponse(response.Result(), tt.code, tt.message, t)
+	}
+}
+
+func verifyResponse(resp *http.Response, expectCode int, expectMessage string, t *testing.T) {
+	b, _ := ioutil.ReadAll(resp.Body)
+	body := strings.Trim(string(b), "\n")
+	if resp.StatusCode != expectCode || body != expectMessage {
+		t.Errorf("expect (%d, %s) "+
+			"got (%d, %s)",
+			expectCode, expectMessage,
+			resp.StatusCode, body)
+	}
+}
+
+func TestErrWrapperInServer(t *testing.T) {
+	for _, tt := range tests {
+		f := errorWrapper(tt.h)
+		server := httptest.NewServer(http.HandlerFunc(f))
+		resp, _ := http.Get(server.URL)
+		verifyResponse(resp, tt.code, tt.message, t)
 	}
 }
