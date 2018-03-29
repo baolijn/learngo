bfa43b00222e359dfbbcdce5d1488ed30d8c38b0
baolijn <libao_fighting@163.com>
2018-03-29 13:51:27 +0800

    add channel as a return and set channel send only

--- channel/channel.go
+++ channel/channel.go
@@ -5,21 +5,28 @@ import (
 	"time"
 )
 
-func work(id int, c chan int)  {
-	func() {
+func createWork(id int) chan<- int {
+	c := make(chan int)
+	go func() {
 		for{
 			fmt.Printf("Worker %d received %c\n", id, <- c)
 		}
 	}()
+	return c
 }
+
 func chanDemo()  {
-	var channels [10]chan int
+	var channels [10]chan<- int
 	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go work(i, channels[i])
+		channels[i] = createWork(i)
 	}
+
 	for i:= 0; i < 10 ;i++  {
 		channels[i] <- 'a' + i
+	}
+
+	for i:= 0; i < 10 ;i++  {
+		channels[i] <- 'A' + i
 	}
 
 	time.Sleep(time.Minute)
