9153ac11eaa505de9ba23f7f97159a01e28e3a0e
baolijn <libao_fighting@163.com>
2018-03-29 14:09:31 +0800

    add bufferedChannel

--- channel/channel.go
+++ channel/channel.go
@@ -5,31 +5,43 @@ import (
 	"time"
 )
 
+func worker(id int, c chan int)  {
+		for {
+			fmt.Printf("Worker %d received %c\n", id, <-c)
+		}
+}
+
 func createWork(id int) chan<- int {
 	c := make(chan int)
-	go func() {
-		for{
-			fmt.Printf("Worker %d received %c\n", id, <- c)
-		}
-	}()
+	go worker(id, c)
 	return c
 }
 
-func chanDemo()  {
+func chanDemo() {
 	var channels [10]chan<- int
 	for i := 0; i < 10; i++ {
 		channels[i] = createWork(i)
 	}
 
-	for i:= 0; i < 10 ;i++  {
+	for i := 0; i < 10; i++ {
 		channels[i] <- 'a' + i
 	}
 
-	for i:= 0; i < 10 ;i++  {
+	for i := 0; i < 10; i++ {
 		channels[i] <- 'A' + i
 	}
 
 	time.Sleep(time.Minute)
+}
+
+func bufferedChannel() {
+	c := make(chan int, 3)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	time.Sleep(time.Millisecond)
 }
 
 func main() {
@@ -38,5 +50,6 @@ func main() {
 	//go func() {c <- 3 + 4}()
 	//i := <- c
 	//fmt.Println(i)
-	chanDemo()
+	//chanDemo()
+	bufferedChannel()
 }
