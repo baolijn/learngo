91aefaa93d45f841d483418be4d9abde1758d677
baolijn <libao_fighting@163.com>
2018-03-29 13:41:33 +0800

    add channel as a parameter

--- channel/channel.go
+++ channel/channel.go
@@ -1,11 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func work(id int, c chan int)  {
+	func() {
+		for{
+			fmt.Printf("Worker %d received %c\n", id, <- c)
+		}
+	}()
+}
+func chanDemo()  {
+	var channels [10]chan int
+	for i := 0; i < 10; i++ {
+		channels[i] = make(chan int)
+		go work(i, channels[i])
+	}
+	for i:= 0; i < 10 ;i++  {
+		channels[i] <- 'a' + i
+	}
+
+	time.Sleep(time.Minute)
+}
 
 func main() {
-	c := make(chan int)
-	defer close(c)
-	go func() {c <- 3 + 4}()
-	i := <- c
-	fmt.Println(i)
+	//c := make(chan int)
+	//defer close(c)
+	//go func() {c <- 3 + 4}()
+	//i := <- c
+	//fmt.Println(i)
+	chanDemo()
 }
