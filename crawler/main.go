fb2ee2edee8503f607c696a65f7afb084482d2e0
baolijn <libao_fighting@outlook.com>
2018-04-05 10:21:26 +0800

    add engine fetcher and parser

--- crawler/main.go
+++ crawler/main.go
@@ -1,43 +1,14 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"io/ioutil"
-	"golang.org/x/text/transform"
-	"io"
-	"golang.org/x/text/encoding"
-	"bufio"
-	"golang.org/x/net/html/charset"
+	"learngo/crawler/engine"
+	"learngo/crawler/zhenai/parser"
 )
 
 func main() {
-	resp, err := http.Get("http://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: status code:", resp.StatusCode)
-		return
-	}
-	e := determineEncoding(resp.Body)
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
-	all, err := ioutil.ReadAll(utf8Reader)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("%s\n", all)
+	engine.Run(engine.Request{
+		Url:	"http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
 
-func determineEncoding(r io.Reader) encoding.Encoding {
-	bytes, err := bufio.NewReader(r).Peek(1024)
-	if err != nil {
-		panic(err)
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
-}
