bd5ce41c103950def7d9efe82f35df971abaaab5
baolijn <libao_fighting@163.com>
2018-04-08 17:23:17 +0800

    extract worker

--- crawler/engine/engine.go
+++ crawler/engine/engine.go
@@ -5,7 +5,7 @@ import (
 	"log"
 )
 
-func Run(seeds ... Request)  {
+func Run(seeds ... Request) {
 	var requests []Request
 	for _, r := range seeds {
 		requests = append(requests, r)
@@ -14,18 +14,26 @@ func Run(seeds ... Request)  {
 	for len(requests) > 0 {
 		r := requests[0]
 		requests = requests[1:]
-		log.Printf("Fetching url %s", r.Url)
-		body, err := fetcher.Fetch(r.Url)
+
+		parseResult, err := worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error fetching url %s %v",
-				r.Url, err)
 			continue
 		}
-		parseResult := r.ParserFunc(body)
 		requests = append(requests, parseResult.Requests...)
 
 		for _, item := range parseResult.Items {
 			log.Printf("Got item %s", item)
 		}
 	}
+}
+
+func worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching url %s", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error fetching url %s %v",
+			r.Url, err)
+		return ParseResult{}, err
+	}
+	return r.ParserFunc(body), nil
 }
