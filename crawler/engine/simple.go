d3aff2ed4cfeb514eb4ddadff38c6b97352eedf9
baolijn <libao_fighting@163.com>
2018-04-11 11:24:37 +0800

    extract worker

--- crawler/engine/simple.go
+++ crawler/engine/simple.go
@@ -31,13 +31,3 @@ func (e SimpleEngine) Run(seeds ... Request) {
 	}
 }
 
-func worker(r Request) (ParseResult, error) {
-	//log.Printf("Fetching url %s", r.Url)
-	body, err := fetcher.Fetch(r.Url)
-	if err != nil {
-		//log.Printf("Fetcher: error fetching url %s %v",
-		//	r.Url, err)
-		return ParseResult{}, err
-	}
-	return r.ParserFunc(body), nil
-}
