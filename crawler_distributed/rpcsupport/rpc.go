38e63546669588451766ef3c9f270736c3bf5111
baolijn <libao_fighting@163.com>
2018-04-12 10:47:12 +0800

    add rpc server and client for distributed crawler

---
+++ crawler_distributed/rpcsupport/rpc.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/rpc"
+	"learngo/rpc"
+	"net"
+	"net/rpc/jsonrpc"
+	"log"
+)
+
+func ServeRpc(host string, service interface{}) error {
+	rpc.Register(rpcdemo.DemoService{})
+
+	listener, err := net.Listen("tcp", host)
+	if err != nil {
+		return err
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("accept error: %v", err)
+			continue
+		}
+
+		go jsonrpc.ServeConn(conn)
+	}
+	return nil
+}
+
+func NewClient(host string) (*rpc.Client, error) {
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonrpc.NewClient(conn), nil
+}
+
